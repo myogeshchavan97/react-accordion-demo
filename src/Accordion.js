e163b6f64247b559b91b15bcfc3f5612943dbdd7
Yogesh <myogeshchavan97@gmail.com>
2021-03-14 16:35:54 +0530

    initial commit

---
+++ src/Accordion.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+const Accordion = ({ title, content }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <div className="accordion-item">
+      <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
+        <div>{title}</div>
+        <div>{isActive ? '-' : '+'}</div>
+      </div>
+      {isActive && <div className="accordion-content">{content}</div>}
+    </div>
+  );
+};
+
+export default Accordion;
